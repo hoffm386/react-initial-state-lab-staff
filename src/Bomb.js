34bdf36439caa5865ade1512a8c5150bdd78ccf3
hoffm386 <erinrhof@uw.edu>
2019-06-25 16:44:11 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
-// your Bomb code here!
+import React, {Component, Fragment} from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+  render() {
+    return (
+        <Fragment>
+          {(() => {
+            if (this.state.secondsLeft === 0) {
+              return "Boom!";
+            } else {
+              return `${this.state.secondsLeft} seconds left before I go boom!`;
+            }
+          })() /* I think the IIFE way is easier to read */}
+        </Fragment>
+    );
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
-// your ImageSlider code here!
+import React, {Component, Fragment} from 'react';
+
+export default class ImageSlider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        I am on slide {this.state.currentSlideIndex}
+      </Fragment>
+    );
+  }
+}
